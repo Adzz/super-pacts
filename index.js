1b9ffe3f1d7a08c6bf11389a007551343e49a48f
michaellennox <michaelctlennox@gmail.com>
2016-04-16 15:34:21 +0100

    can grab stuff frmo mondo yay

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+'use strict'
+
 const express = require("express");
 const handlebars = require("express-handlebars");
-const https = require("https");
+const request = require('request')
 
 const app = express();
-
-
 
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
-
 
 app.get("/", (req, res)=>{
   res.render("index");
@@ -19,18 +18,16 @@ const accountID = "acc_000097FmTS5YV5JkSUhapF";
 const accessToken= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaSI6Im9hdXRoY2xpZW50XzAwMDA5NFB2SU5ER3pUM2s2dHo4anAiLCJleHAiOjE0NjA5ODE5NTUsImlhdCI6MTQ2MDgwOTE1NSwianRpIjoidG9rXzAwMDA5N0Z4MWJpV0N2dGZRYWZwN0IiLCJ1aSI6InVzZXJfMDAwMDk2RmFTbUlhZDFUZTRZSGRGaCIsInYiOiIyIn0.H8aYQZk5Z-KFec6SlJCP0q_8rFvhXYX3zLKldCQi4UI";
 
 const options = {
-  hostname: "api.getmondo.co.uk",
-  path: "/transactions?account_id=" + accountID,
-  method: "GET",
+  url: "https://api.getmondo.co.uk/transactions?account_id=" + accountID,
   headers: {
     "Authorization": "Bearer " + accessToken
     }
 };
 
-app.get("/fails", (req, res)=>{
-	https.request(options, (data)=>{
-		res.json(data);
-	})
+app.get("/fails", (req, res) => {
+  request(options, (err, response, body) => {
+    res.json(body);
+  });
 });
 
 app.listen(8080);
