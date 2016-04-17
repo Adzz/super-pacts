c9c2834acbdea0d43ec93d929e292469e798dda8
michaellennox <michaelctlennox@gmail.com>
2016-04-17 13:20:51 +0100

    Adds twitter integration to inform people they failed

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const _ = require('lodash');
 const app = express();
 const pledgeFeed = require('./lib/pledgeFeedReader').displayMostUrgentPledges;
 const todayPledgeFeed = require('./lib/pledgeFeedReader').filterPledgesDueToday;
-const twitterClient = require('./lib/peepPeep')
+const twitterClient = require('./lib/peepPeep');
+
+const tweeter = new twitterClient({
+  consumer_key: 'TFPseWmz85EDXnmGcEYxZ7gFE',
+  consumer_secret: '0NZshRBV87GhkCYXA1u1qppI8ssyHI3Qq5rkk309WUE4n1KAqj',
+  access_token: '721630475148730368-iMskDeFiwt9z6g1DPOUfeGH7ytQxPON',
+  access_token_secret: 'ei9ffvAoBMrNSmKwg2lg6v1oUhG4zDH7278po6VxcD9p0'
+});
 
 if (!secrets || !secrets.accountID || !secrets.accessToken) {
   console.error('Missing parameters, check your secrets.json');
@@ -47,7 +54,7 @@ app.get('/home', (req, res) => {
 });
 
 app.get('/admin', (req, res) => {
-  firebaseClient.getAllPledges((data) => {
+  firebaseClient.getAllPledges( data => {
     const pledges = todayPledgeFeed(data);
     res.render('index', { pledges });
   });
@@ -79,6 +86,11 @@ function routeMondoHooks(notes) {
     return firebaseClient.setPledgeHonoured(user);
   } else if (_.endsWith(notes, 'failed')) {
     console.log(user + ' has failed to meet their pledge');
+    firebaseClient.getLastPledgeFor(user, data => {
+      const amount = data.amount;
+      const twitter = data.twitter;
+      tweeter.sendFailureMessage(twitter, amount)
+    });
   } else {
     console.log(user + ' has paid their pledge');
     firebaseClient.registerPaid(user);
